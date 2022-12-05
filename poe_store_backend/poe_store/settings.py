e5ee6bff71c990f3a17470f88f2e5b5548a20999
johnsonas1995 <johnsonas1995@gmail.com>
2022-12-05 15:46:50 -0500

    security update

--- poe_store_backend/poe_store/settings.py
+++ poe_store_backend/poe_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p^^d8eowo__ql-u5%%x80vf=qrye@881@vr8*6qf@(y0#3&(b9'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
