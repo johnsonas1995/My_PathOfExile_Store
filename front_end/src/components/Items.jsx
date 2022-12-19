cf67ae615af93a9bd9301e6dd2ba8fb6f7ba3a23
johnsonas1995 <johnsonas1995@gmail.com>
2022-12-19 11:32:12 -0500

    card styling and separated items into component

---
+++ front_end/src/components/Items.jsx
@@ -0,0 +1,45 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from 'react-bootstrap/Card';
+
+function Items(props) {
+    return (
+        <div className="cards" >
+        {props.stashItems &&
+            props.stashItems.map((item) => {
+            return (
+                <div >
+                <Card className="cardItem border-light mb-3 style={{ width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}">
+                <Card.Body className="card-body">
+                <Card.Header>
+                    <Card.Img class="item" src={item.icon} />
+                    <br/>
+                    <button className="button">Add To Cart</button>
+                    <br/>
+                    <button className="button">Details</button>
+                    </Card.Header>
+                    
+                    <Card.Title className="card-header">{item.name}</Card.Title>
+                    <Card.Title className="card-header">{item.baseType}</Card.Title>
+                    <Card.Text className="card-text">
+                    <h6>Explicit Modifiers:</h6>
+                    {item.explicitMods &&
+                        item.explicitMods.map((mod) => {
+                        return (
+                            <>
+                            <>{mod}</>
+
+                            </>
+                        );
+                        })}
+                    <h6>Implicit Modifiers:</h6>
+                    {item.implicitMods}
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+                </div>
+                
+            );
+            })}
+            </div>)
+}
+export default Items;
--- front_end/src/components/LiveTabBrowser.jsx
+++ front_end/src/components/LiveTabBrowser.jsx
@@ -2,6 +2,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Card from 'react-bootstrap/Card';
 import { useState } from "react";
 import axios from "axios";
+import Items from './Items.jsx'
 
 function LiveTabBrowser(props) {
   const [stashItems, setStashItems] = useState([]);
@@ -12,8 +13,8 @@ function LiveTabBrowser(props) {
 
   function getNumStashTabs() {
     axios.get("stashes/" + props.league + "/0/").then((response) => {
-      console.log(response.data.response["numTabs"]);
-      console.log(response.data.response.tabs);
+      // console.log(response.data.response["numTabs"]);
+      // console.log(response.data.response.tabs);
       setNumTabs(response.data.response["numTabs"]);
       setStashTabs(response.data.response.tabs);
     });
@@ -21,19 +22,21 @@ function LiveTabBrowser(props) {
 
   function getStashTab(tabIndex) {
     axios.get("stashes/" + props.league + "/" + tabIndex + "/").then((response) => {
-      console.log(response.data.response.items);
-      console.log(response.data.response);
+      // console.log(response.data.response.items);
+      // console.log(response.data.response);
       // let { tabIndex } = useParams()
-      setStashItems(response.data.response.items);
+      if (response.data.response.items.length > 0) {
+        setStashItems(response.data.response.items)
+      }
+    
+      console.log(stashItems)
     });
   }
 
   return (
-      <div> 
-            
+      <div>  
         {props.user ? <div>
            <h4>Select League:</h4>
-            
           <div
             onChange={(event) => {
               props.setLeague(event.target.value);
@@ -60,49 +63,19 @@ function LiveTabBrowser(props) {
           <br/>
           <br/>
           <p>
-            {numTabs} Tabs in {props.league} league
+            {numTabs} Tabs in {props.league} league <br/>
+            Brows Tabs below:
           </p>
           {stashTabs && stashTabs.map((stashTab) => {
             return (
-              <div>
-                <Card className="card card border-light mb-3">
-                  <button className="button" onClick={() => getStashTab(stashTab.i)}>
-                    Tab:{stashTab.i} ({stashTab.n})
-                  </button>
-                </Card>
-                  
-                  {stashItems &&
-                    stashItems.map((item) => {
-                      return (
-                        <div>
-                          <Card className="cardItem border-light mb-3">
-                            <Card.Img class="item" src={item.icon} />
-                              <Card.Body className="card-body">
-                              <Card.Title className="card-header">{item.name}</Card.Title>
-                              <Card.Title className="card-header">{item.baseType}</Card.Title>
-                              <Card.Text className="card-text">
-                              <h6>Explicit Modifiers:</h6>
-                              {item.explicitMods &&
-                                item.explicitMods.map((mod) => {
-                                  return (
-                                    <>
-                                      <>{mod}</>
-    
-                                    </>
-                                  );
-                                })}
-            
-                              <h6>Implicit Modifiers:</h6>
-                              {item.implicitMods}
-                              </Card.Text>
-                              </Card.Body>
-                          </Card>
-                          </div>
-                      );
-                    })}
-              </div>
+              <>
+                    <button className="button" onClick={() => getStashTab(stashTab.i)}>
+                    {stashTab.n}
+                    </button>
+              </>
             );
           })}
+          <Items stashItems={stashItems}/>
         </div> : <>
         <>You must be signed in to view this content.</> <br/>
         <> <a href='/signIn' >Click here to sign In</a><br/>
